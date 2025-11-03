51eabef71a8d01b252549662caddd18ac1d91779
Eric-Coutinho <Eric.Coutinhop@hotmail.com>
2025-11-03 18:21:26 -0300

    Adding App.tsx for navigation, creating Eric branch

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
+// App.tsx
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import Home from '../screens/Home';
+import Home from './screens/Home';
 
 const Stack = createNativeStackNavigator();
 
-export default function Index() {
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
+        {/* outras telas */}
       </Stack.Navigator>
     </NavigationContainer>
   );
