7557cfe6c7befa4592c20a818e7f9fa3b29b9721
Eric-Coutinho <Eric.Coutinhop@hotmail.com>
2025-11-04 20:52:12 -0300

    creating first page, added navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,46 @@
-import { Text, View } from "react-native";
+import { Image } from "expo-image";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.banner}>
+        <Image
+          source={require("../assets/images/Vila Rica.jpeg")}
+          style={styles.image}
+        />
+      </View>
+      <View style={styles.body}>
+        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Link href="/home" style={styles.button}>
+          Ir para a home
+        </Link>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+  body: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  button: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "#007AFF",
+    borderRadius: 5,
+  },
+  banner: {
+    maxWidth: 1000
+  },
+  image: {
+    width: 500,
+    height: 200,
+    objectFit: "fill",
+  },
+});
